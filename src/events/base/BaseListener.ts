4a4aef147eef551966c8392b392e1a86083eb0f9
VanyaY <ivanvyakovlev@gmail.com>
2020-12-06 02:40:58 +0300

    refactor(events): make client field protected

--- src/events/base/BaseListener.ts
+++ src/events/base/BaseListener.ts
@@ -14,7 +14,7 @@ export abstract class BaseListener<T extends Event> {
 
   protected ackWait = 5000
 
-  constructor(private client: Stan) {}
+  constructor(protected client: Stan) {}
 
   subscriptionOptions() {
     return this.client
