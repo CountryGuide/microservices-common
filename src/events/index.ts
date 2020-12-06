43b2637e763f41a786af12cbefca969218e64151
VanyaY <ivanvyakovlev@gmail.com>
2020-12-06 15:55:23 +0300

    feat(events): add ExpirationCompleteEvent

--- src/events/index.ts
+++ src/events/index.ts
@@ -9,4 +9,6 @@ export { TicketUpdatedEvent } from './tickets/TicketUpdatedEvent'
 export { OrderCreatedEvent } from './orders/OrderCreatedEvent'
 export { OrderCancelledEvent } from './orders/OrderCancelledEvent'
 
+export { ExpirationCompleteEvent } from './expiration/ExpirationCompleteEvent'
+
 export { OrderStatus } from './types/OrderStatus'
